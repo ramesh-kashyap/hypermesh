771b449525b1d60cf32f2db09265d8d95a7741d6
ramesh-kashyap <rameshkashyap8801@gmail.com>
2025-02-15 18:00:46 +0530

    done

--- src/pages/invest/RechargeFunds.js
+++ src/pages/invest/RechargeFunds.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const RechargeFunds = () => {
     const [isCollapsed, setIsCollapsed] = useState(true);
@@ -27,13 +28,17 @@ const RechargeFunds = () => {
             <div>
                 <div className="flex justify-between">
                     <span className="text-white text-sm rounded-[22px] h-[33px] px-3 bg-black mb-4 mt-10 flex items-center space-x-2 cursor-pointer">
-                        <img alt="Back Icons" loading="lazy" width="17" height="12" src="/upnl/assets/icons/icon-back.svg" style={{ color: 'transparent' }} />
-                        <span>Back</span>
+                    
+                        <img alt="Back Icons" loading="lazy" width="17" height="12" src="/upnl/assets/icons/icon-back.svg" style={{ color: '#fff' }} />
+                        <Link to='/Wallet'>
+                        <span style={{color:'#fff'}}>Back</span>
+                        </Link>
                     </span>
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-5 xl:grid-cols-3 gap-6">
                     <div className="lg:col-span-3 xl:col-span-2 bg-white rounded-[16px] p-6">
                         <h2 className="text-xl sm:text-2xl font-bold">Deposit</h2>
+                        <br></br>
                         <div className="flex justify-between items-center w-full bg-[#F9F9F9] rounded-[27px] px-[40px] max-sm:px-[20px] py-[4px] max-sm:py-[10px] text-[14px] text-[#999999] my-4 sm:my-6 sm:py-6 max-sm:gap-0">
                             <span className="text-center text-[#171717] cursor-pointer" onClick={toggleBinanceVisibility}>Select token</span>
                             <img alt="right-arrow" loading="lazy" width="30" height="30" src="/upnl/assets/icons/right-arrow.svg" style={{ color: 'transparent' }} />
@@ -46,10 +51,10 @@ const RechargeFunds = () => {
                             {/* Collapsed Div */}
                             <div className="flex items-center justify-between bg-[#F9F9F9] h-[72px] mb-2 p-[15px] rounded-[16px] cursor-pointer hover:bg-[#ebe8e8]" onClick={toggleCollapse}>
                                 <div className="py-4 flex items-center space-x-2 lg:space-x-3 text-sm">
-                                    <img alt="BNB logo" loading="lazy" width="40" height="40" src="/upnl/assets/icons/logo_bnb_2.svg" style={{ color: 'transparent' }} />
+                                    <img alt="BNB logo" loading="lazy" width="40" height="40" src="/upnl/assets/icons/logo_bnb_2.svg" style={{ color: 'transparent', marginLeft: '10px', width: '40px' }} />
                                     <div>
-                                        <div className="text-[16px] text-[#171717] font-semibold">BNB</div>
-                                        <div className="text-[12px] text-[#999999]">BNB</div>
+                                        <div className="text-[16px] text-[#171717] font-semibold">USDT (BEP20)</div>
+                                        <div className="text-[12px] text-[#999999]">Binance Smart Chain</div>
                                     </div>
                                 </div>
                                 <img alt="BNB logo" loading="lazy" width="20" height="20" src="/upnl/assets/icons/right-repo.svg" style={{ color: 'transparent' }} />
@@ -58,12 +63,103 @@ const RechargeFunds = () => {
                             {/* Expanded Div */}
                             {!isCollapsed && (
                                 <div className="flex items-center justify-between bg-[#F9F9F9] h-[120px] mb-2 p-[15px] rounded-[16px] cursor-pointer hover:bg-[#ebe8e8]" onClick={toggleCollapse}>
-                                    <div className="py-4 flex items-start space-x-2 lg:space-x-3 text-sm">
-                                        <img alt="BSC logo" loading="lazy" width="40" height="40" src="/upnl/assets/icons/logo_bsc_2.svg" style={{ color: 'transparent' }} />
+                                    <div className="py-4 flex items-start space-x-2 lg:space-x-3 text-sm" style={{ marginLeft: '10px' }}>
+
                                         <div>
-                                            <div className="text-[16px] text-[#171717] font-semibold mb-2">Binance Smart Chain</div>
-                                            <div className="text-[12px] text-[#999999]">1 block confirmations</div>
-                                            <div className="text-[12px] text-[#999999]">Estimated time up to 1 minute</div>
+                                            {/* Separator Line with BNB Label */}
+                                            <div className="relative py-[20px] md:py-[30px]">
+                                                <div className="w-full h-[2px] bg-[#DFDFDF]"></div>
+                                                <div className="absolute flex items-center gap-1 translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] bg-white px-2">
+                                                    <img
+                                                        alt="BNB logo"
+                                                        loading="lazy"
+                                                        width="30"
+                                                        height="30"
+                                                        decoding="async"
+                                                        src="/upnl/assets/icons/logo_bnb_2.svg"
+                                                        style={{ color: "transparent" }}
+                                                    />
+                                                    <span className="text-[20px] font-bold">BNB</span>
+                                                </div>
+                                            </div>
+
+                                            {/* Deposit Section */}
+                                            <div className="flex flex-col items-center">
+                                                <div className="grid grid-cols-3 gap-4 w-full mt-5 p-6 bg-[#F9F9F9] rounded-[20px]">
+                                                    <div className="col-span-3 text-center text-[#999999] text-[14px]">
+                                                        Send BNB tokens to the QR code or address below.
+                                                    </div>
+
+                                                    {/* QR Code */}
+                                                    <div className="col-span-2 md:col-span-1">
+                                                        <img
+                                                            style={{
+                                                                width: "200px",
+
+                                                                margin: "0px auto",
+                                                            }}
+                                                            src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=0x1A74f5d2D0209A1D9C58a70cc75d9CFC74E70fcC"
+                                                        />
+                                                    </div>
+
+                                                    {/* Network & Address Section */}
+                                                    <div className="col-span-3 md:col-span-2 h-full flex flex-col justify-start gap-4">
+                                                        {/* Network Information */}
+                                                        <div className="bg-white p-4 w-full rounded-[20px]">
+                                                            <p className="text-secondary text-[14px] font-medium pb-[7px]">Network</p>
+                                                            <div className="flex items-center justify-start gap-2">
+                                                                <img
+                                                                    alt="BSC logo"
+                                                                    loading="lazy"
+                                                                    width="30"
+                                                                    height="30"
+                                                                    decoding="async"
+                                                                    src="/upnl/assets/icons/logo_bnb_2.svg"
+                                                                    style={{ color: "transparent" }}
+                                                                />
+                                                                Binance Smart Chain
+                                                            </div>
+                                                        </div>
+
+                                                        {/* Wallet Address */}
+                                                        <div className="bg-white p-4 w-full rounded-[20px] flex items-center justify-between">
+                                                            <div
+                                                                className="break-all text-secondary text-[14px] font-medium flex flex-col gap-1"
+                                                                style={{ width: "calc(100% - 100px)" }}
+                                                            >
+                                                                <p>Your Address</p>
+                                                                <p className="text-primary">
+                                                                    0xa49E83Eba92DE67387e5CCC7dCa12782bEE618eb
+                                                                </p>
+                                                            </div>
+                                                            <button className="bg-black min-w-[90px] h-[46px] rounded-[30px] text-white px-4 py-2" style={{ color: "#fff" }}>
+                                                                Copy
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            {/* Warning Section */}
+                                            <div className="flex justify-between items-center gap-4 w-full border mt-5 p-3 rounded-[20px]">
+                                                <img
+                                                    alt="warning logo"
+                                                    loading="lazy"
+                                                    width="40"
+                                                    height="40"
+                                                    decoding="async"
+                                                    src="/upnl/assets/icons/icon_warning_2.svg"
+                                                    style={{ color: "transparent" }}
+                                                />
+                                                <p className="text-sm text-secondary">
+                                                    Important: Keep this page open until your deposit transaction is
+                                                    recorded. If it's not recorded, you can use our form to{" "}
+                                                    <a href="#" className="text-primary">
+                                                        submit it manually
+                                                    </a>
+                                                    .
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
                                     <img alt="BSC logo" loading="lazy" width="20" height="20" src="/upnl/assets/icons/right-repo.svg" style={{ color: 'transparent' }} />
@@ -73,10 +169,10 @@ const RechargeFunds = () => {
                             {/* USDT Section */}
                             <div className="flex items-center justify-between bg-[#F9F9F9] h-[72px] mb-2 p-[15px] rounded-[16px] cursor-pointer hover:bg-[#ebe8e8]" onClick={toggleBinanceVisibility}>
                                 <div className="py-4 flex items-center space-x-2 lg:space-x-3 text-sm">
-                                    <img alt="USDT logo" loading="lazy" width="40" height="40" src="/upnl/assets/icons/logo_usdt_2.svg" style={{ color: 'transparent' }} />
+                                    <img alt="USDT logo" loading="lazy" width="40" height="40" src="/upnl/assets/icons/logo_usdt_2.svg" style={{ color: 'transparent', marginLeft: '10px', width: '40px' }} />
                                     <div>
-                                        <div className="text-[16px] text-[#171717] font-semibold">USDT</div>
-                                        <div className="text-[12px] text-[#999999]">TetherUS</div>
+                                        <div className="text-[16px] text-[#171717] font-semibold">USDT (TRC20)</div>
+                                        <div className="text-[12px] text-[#999999]">Tron Blockchain</div>
                                     </div>
                                 </div>
                                 <img alt="USDT logo" loading="lazy" width="20" height="20" src="/upnl/assets/icons/right-repo.svg" style={{ color: 'transparent' }} />
